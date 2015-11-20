d152ec5acc62baa8e61d10601d2c9da4df835070
Ben Medcalf <ben.medcalf@gmail.com>
2015-11-20 17:36:28 -0500

    Forgot to do a ton of shit in the onCreateView method.
    -added a reference to GridView so I could attach the ImageAdapter which I also added.
    -Added the toast

--- Quadrants-2/app/src/main/java/com/example/ben/quadrants_2/ImageGridFragment.java
+++ Quadrants-2/app/src/main/java/com/example/ben/quadrants_2/ImageGridFragment.java
@@ -15,6 +15,8 @@ import android.widget.Toast;
  */
 public class ImageGridFragment extends Fragment {
 
+    private ImageAdapter mImageAdapter;
+
     public ImageGridFragment() {
 
     }
@@ -25,15 +27,30 @@ public class ImageGridFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
 
-        final Context context = getActivity();
-
-
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        final Context context = getActivity();
+
+        mImageAdapter = new ImageAdapter(context);
+
+        // Create the rootView
         View rootView = inflater.inflate(R.layout.image_grid_fragment, container, false);
+
+        //Get a reference to the GridView and attach this adapter to it
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridView);
+        gridView.setAdapter(mImageAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(context,"" + position, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Inflate the layout for this fragment
         return rootView;
     }
 }
